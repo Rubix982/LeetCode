acb04320b85fe99fc2eae5907bbbd7a8386425ab
Saif Ul Islam <k180307@nu.edu.pk>
2022-05-02 08:28:32 +0500

    Time: 2 ms (95.98%), Space: 54.9 MB (38.04%) - LeetHub

--- 645-set-mismatch/645-set-mismatch.java
+++ 645-set-mismatch/645-set-mismatch.java
@@ -1,8 +1,6 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
         
-        int[] result = new int[2];
-        
         boolean[] numBool = new boolean[nums.length];
         int duplicate = -1;
         int toReplaceWith = -1;
@@ -17,22 +15,14 @@ class Solution {
             } else {
                 duplicate = nums[i];
             }
-        }
-        
-        for (int i = 0; i < nums.length; i++) {
-            System.out.println(numBool[i]);
-        }
-        
+        }        
         
         for (int i = 0 ; i<nums.length; i++) {
             if (numBool[i] == false) {
                 toReplaceWith = i;
             }
         }
-        
-        // System.out.println(toReplaceWith + 1);     
-        // System.out.println(duplicate + 1);        
-        
+
         return new int[]{duplicate, toReplaceWith + 1};
     }
 }
