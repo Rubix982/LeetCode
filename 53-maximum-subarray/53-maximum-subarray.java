16832531c3fa0aeb53e9ea014fb9130397751994
Saif Ul Islam <k180307@nu.edu.pk>
2022-05-03 09:18:31 +0500

    Time: 2 ms (56.03%), Space: 74.4 MB (31.14%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,18 +1,19 @@
 class Solution {    
     public int maxSubArray(int[] nums) {
-        
-//         if (nums.length == 1) {
-//             return nums[0];
-//         }
-        
-        int[] dp = new int[nums.length];
+        // int[] dp = new int[nums.length];
 
-        dp[0] = nums[0];
-        int max = dp[0];
+        // dp[0] = nums[0];
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
         
-        for (int i = 1; i < nums.length; i++) {
-            dp[i] = Math.max(dp[i-1] + nums[i], nums[i]);
-            max = Math.max(dp[i], max);
+        for (int i = 0; i < nums.length; i++) {
+            // dp[i] = Math.max(dp[i-1] + nums[i], nums[i]);
+            // max = Math.max(dp[i], max);
+            sum += nums[i];
+            
+            max = Math.max(max, sum);
+            
+            if (sum < 0) { sum = 0; }
         }
         return max;
     }
