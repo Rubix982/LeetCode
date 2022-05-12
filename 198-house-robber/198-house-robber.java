1be9bbdf7ad3a08e816e41b428330d60681b181f
Saif Ul Islam <k180307@nu.edu.pk>
2022-05-12 08:23:27 +0500

    Time: 0 ms (100.00%), Space: 41.6 MB (38.93%) - LeetHub

---
+++ 198-house-robber/198-house-robber.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length==1) { return nums[0]; }
+        
+        int prev = nums[0], secondPrev = 0, current;
+        
+        for(int i = 1; i < nums.length; i++){
+            
+            current = nums[i] + secondPrev;
+            current = Math.max(current, prev);
+            
+            secondPrev = prev;
+            prev = current;
+        }
+        return prev;        
+    }
+}
