d4332b1274cb6b082fa58eeb11efdd676c9cde1f
Saif Ul Islam <k180307@nu.edu.pk>
2022-08-15 22:40:36 +0500

    Time: 2 ms (68.46%), Space: 41.4 MB (39.05%) - LeetHub

---
+++ 202-happy-number/202-happy-number.java
@@ -0,0 +1,27 @@
+import java.lang.*;
+
+class Solution {
+    public boolean isHappy(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        
+        if (n == 1) {
+            return true;
+        }
+        
+        int sum = 0;
+
+        while (n >= 10) {            
+            for (sum = 0; n > 0; n /= 10) {
+                sum += (n % 10) * (n % 10);
+                if (sum > Integer.MAX_VALUE) {
+                    return false;
+                }
+            }
+            n = sum;
+        }
+        
+        return n == 1 || n == 7;
+    }
+}
