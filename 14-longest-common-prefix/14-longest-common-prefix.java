1d3bf46ea63d685262ede20e8d3141de3a55b7de
Saif Ul Islam <k180307@nu.edu.pk>
2022-08-18 13:34:58 +0500

    Time: 2 ms (50.84%), Space: 42.3 MB (46.79%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -0,0 +1,62 @@
+class Solution {
+    
+    public boolean outOfBoundsStringLength(int endingIndex, int strLength) {
+        if (endingIndex == strLength || endingIndex + 1 == strLength) {
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public String longestCommonPrefix(String[] strs) {
+   
+        if (strs.length == 0) {
+            return "";
+        }
+        
+        if (strs.length == 1) {
+            return strs[0];
+        }
+
+        for (int i = 0; i < strs.length; i++) {    
+            if (strs[i].length() == 0) {
+                return "";
+            }
+        }
+        
+        int endingIndex = 0;
+        boolean hasPrefixMaxLengthReached = false;
+        
+        while (true) {
+
+            for (int i = 0; i < strs.length; i++) {
+                if (endingIndex > strs[i].length() - 1) {
+                    hasPrefixMaxLengthReached = true;
+                    break;
+                }
+            }
+            
+            if (hasPrefixMaxLengthReached) {
+                break;
+            }
+            
+            char posChar = strs[0].charAt(endingIndex);
+            boolean prefixEndReached = false;
+            
+            for (int i = 1; i < strs.length; i++) {
+                if (posChar != strs[i].charAt(endingIndex)) {
+                    prefixEndReached = true;
+                    break;
+                }
+            }
+            
+            if (prefixEndReached) {
+                break;
+            }
+            
+            endingIndex += 1;
+        }
+        
+        return strs[0].substring(0, endingIndex);
+    }
+}
