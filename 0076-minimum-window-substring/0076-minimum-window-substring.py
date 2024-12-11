9b507f44b2fb2caac49e9962c5e360672d17a340
Saif Ul Islam <k180307@nu.edu.pk>
2024-12-11 10:09:57 +0300

    Time: 99 ms (46.04%), Space: 17.7 MB (18.65%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,41 +1,42 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-
-        hash_map: dict = {}
-
-        for c in t:
-            if c in hash_map:
-                hash_map[c] += 1
-            else:
-                hash_map[c] = 1
+        from collections import Counter
+        
+        # Count the characters in t
+        required = Counter(t)
+        window = Counter()
+        
+        # Number of unique characters in t that need to match in the window
+        required_chars = len(required)
+        formed_chars = 0
 
         start, end = 0, 0
-
-        min_window_length = len(s) + 1
-
-        number_of_chars_to_be_included = len(t)
+        min_window_length = float('inf')
+        min_window_start = 0
 
         while end < len(s):
-            if s[end] in hash_map:
-                if hash_map[s[end]] > 0:
-                    number_of_chars_to_be_included -= 1
-                hash_map[s[end]] -= 1
+            char = s[end]
+            window[char] += 1
+            
+            # If the current character matches the count in `required`, it's fully formed
+            if char in required and window[char] == required[char]:
+                formed_chars += 1
 
-            while number_of_chars_to_be_included == 0:
+            # Try shrinking the window if all required characters are formed
+            while formed_chars == required_chars:
                 if end - start + 1 < min_window_length:
                     min_window_length = end - start + 1
                     min_window_start = start
 
-                if s[start] in hash_map:
-                    hash_map[s[start]] += 1
-                    if hash_map[s[start]] > 0:
-                        number_of_chars_to_be_included += 1
-
+                # Remove the leftmost character
+                left_char = s[start]
+                window[left_char] -= 1
+                if left_char in required and window[left_char] < required[left_char]:
+                    formed_chars -= 1
+                
                 start += 1
 
             end += 1
 
-        if min_window_length == len(s) + 1:
-            return ""
-        else:
-            return s[min_window_start:min_window_start + min_window_length]
+        # Return the smallest window
+        return "" if min_window_length == float('inf') else s[min_window_start:min_window_start + min_window_length]
