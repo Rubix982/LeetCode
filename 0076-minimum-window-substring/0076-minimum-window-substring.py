41ffad5a0465ab73d876f88680d3e46c7f818b89
Saif Ul Islam <k180307@nu.edu.pk>
2024-12-11 10:16:02 +0300

    Time: 47 ms (95.88%), Space: 17.6 MB (21.51%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -15,21 +15,24 @@ class Solution:
         while end < len(s):
             char_end = s[end]
             # If character is in t, decrement its count in the bitmap
-            if required[ord(char_end)] > 0:
+            ord_char_end = ord(char_end)
+            if required[ord_char_end] > 0:
                 number_of_chars_to_be_included -= 1
-            required[ord(char_end)] -= 1
+            required[ord_char_end] -= 1
 
             # Shrink the window when all characters are matched
             while number_of_chars_to_be_included == 0:
                 # Update the minimum window
-                if end - start + 1 < min_window_length:
-                    min_window_length = end - start + 1
+                length_calc = end - start + 1
+                if length_calc < min_window_length:
+                    min_window_length = length_calc
                     min_window_start = start
 
                 # Remove the start character from the window
                 char_start = s[start]
-                required[ord(char_start)] += 1
-                if required[ord(char_start)] > 0:
+                ord_char_start = ord(char_start)
+                required[ord_char_start] += 1
+                if required[ord_char_start] > 0:
                     number_of_chars_to_be_included += 1
 
                 start += 1
