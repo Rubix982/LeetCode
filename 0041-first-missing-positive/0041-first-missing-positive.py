db7f10e14c83dcfa78a1b462d2e8cc56b2a14811
Saif Ul Islam <k180307@nu.edu.pk>
2024-12-14 09:52:08 +0300

    Time: 21 ms (89.1%), Space: 33.9 MB (15.08%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,24 +1,9 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        # nums = set(nums)
-        # smallest_positive = 1
+        nums = set(nums)
+        smallest_positive = 1
 
-        # while smallest_positive in nums:
-        #     smallest_positive += 1
+        while smallest_positive in nums:
+            smallest_positive += 1
 
-        # return smallest_positive
-
-        n = len(nums)
-
-        # Place each number in its correct position if possible
-        for i in range(n):
-            while 1 <= nums[i] <= n and nums[nums[i] - 1] != nums[i]:
-                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
-
-        # Find the first missing positive
-        for i in range(n):
-            if nums[i] != i + 1:
-                return i + 1
-
-        # If all positions are filled correctly, the missing number is n + 1
-        return n + 1
+        return smallest_positive
