089b5671b1f2816fc67ba136b11fc41eee2e7ffb
Saif Ul Islam <k180307@nu.edu.pk>
2024-12-16 20:25:14 +0300

    Time: 0 ms (100%), Space: 17.6 MB (11.77%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.py
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.py
@@ -1,5 +1,6 @@
 class Solution:
     def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+        min_val, min_idx = 2 ** 31 - 1, -1
         for i in range(k):
             min_val, min_idx = 2 ** 31 - 1, -1
             for jdx, val in enumerate(nums):
