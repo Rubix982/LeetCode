0012f10040f336654725c3e673847226ca9beb17
Saif Ul Islam <k180307@nu.edu.pk>
2024-12-17 19:33:25 +0300

    Time: 2 ms (84.87%), Space: 17.6 MB (6.78%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,30 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        if not s:
+            return 0
+
+        roman_to_val = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+
+        if len(s) == 1:
+            return roman_to_val[s[0]]
+
+        roman_sum: int = 0
+        prev_val: int = 0
+
+        for char in reversed(s):
+            curr_val = roman_to_val[char]
+            if curr_val < prev_val:
+                roman_sum -= curr_val
+            else:
+                roman_sum += curr_val
+            prev_val = curr_val
+
+        return roman_sum
