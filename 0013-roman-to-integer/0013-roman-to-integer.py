4124a82f5aab10a6c080fb708147c5cf1957d4b3
Saif Ul Islam <k180307@nu.edu.pk>
2024-12-17 19:36:24 +0300

    Time: 8 ms (28.68%), Space: 17.7 MB (6.78%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -21,10 +21,9 @@ class Solution:
 
         for char in reversed(s):
             curr_val = roman_to_val[char]
-            if curr_val < prev_val:
-                roman_sum -= curr_val
-            else:
-                roman_sum += curr_val
+            mutliplier = -1 if curr_val < prev_val else 1
+            curr_val = curr_val * mutliplier
+            roman_sum = roman_sum + curr_val
             prev_val = curr_val
 
         return roman_sum
