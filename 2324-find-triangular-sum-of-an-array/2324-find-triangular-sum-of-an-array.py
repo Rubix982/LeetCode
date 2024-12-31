195c5e23e733e2b93aa36a0afe9c1a2022f70db3
Saif Ul Islam <k180307@nu.edu.pk>
2025-01-01 00:02:06 +0500

    Time: 1190 ms (60.19%), Space: 17.9 MB (23.89%) - LeetHub

--- 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.py
+++ 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.py
@@ -1,5 +1,12 @@
 class Solution:
     def triangularSum(self, nums: List[int]) -> int:
+        n = len(nums)
+        for _ in range(n - 1):
+            for i in range(n - 1):
+                nums[i] = (nums[i] + nums[i + 1]) % 10
+            n -= 1  # Reduce the effective size of the array
+        return nums[0]
+
         while len(nums) != 1:
             nums = [(nums[i] + nums[i+1]) % 10 for i in range(len(nums) - 1)]
 
