efb6cf4b6a0dade4e11f4a3d5011aa5741cfd531
Saif Ul Islam <k180307@nu.edu.pk>
2024-12-31 23:46:22 +0500

    Time: 0 ms (100%), Space: 17.9 MB (8.08%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -12,9 +12,8 @@ class Solution:
 
             last_level = results[-1]
             level_results = [1]
-            for j in range(len(last_level)):
-                if j + 1 != len(last_level):
-                    level_results.append(last_level[j] + last_level[j+1])
+            for j in range(0, len(last_level) - 1):
+                level_results.append(last_level[j] + last_level[j+1])
 
             level_results.append(1)
             results.append(level_results)
