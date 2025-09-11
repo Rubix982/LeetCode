2e26640a2432ed5cd49405cb86dece1f91054ad6
Saif Ul Islam <k180307@nu.edu.pk>
2025-09-11 20:06:38 +0500

    Time: 6 ms (92.06%), Space: 15.4 MB (96.41%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
@@ -8,7 +8,7 @@ public:
         bool isVowel[123] = {0};
         for (char c : "aeiouAEIOU") isVowel[(unsigned char)c] = true;
 
-        std::vector<char> vowelChars;
+        std::string vowelChars;
         vowelChars.reserve(s.size());
 
         for (char c : s) if (isVowel[(unsigned char)c]) vowelChars.push_back(c);
@@ -16,7 +16,7 @@ public:
         std::sort(vowelChars.begin(), vowelChars.end());
 
         int idx = 0;
-        for (char &c: s) if (isVowel[(unsigned char)c]) c = vowelChars.at(idx++);
+        for (char &c: s) if (isVowel[(unsigned char)c]) c = vowelChars[idx++];
 
         return s;
     }
