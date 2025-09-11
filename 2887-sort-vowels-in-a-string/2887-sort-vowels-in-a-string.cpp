2ff809a92acccbb0abdcbaf849bf0576d755af71
Saif Ul Islam <k180307@nu.edu.pk>
2025-09-11 19:58:38 +0500

    Time: 30 ms (10.44%), Space: 17.8 MB (17.91%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
@@ -1,32 +1,25 @@
-#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
 
 class Solution {
 public:
     string sortVowels(string s) {
-        char vowels[10] = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
-        std::vector<char> vowelCharactersOnly;
-        for (char strChar : s) {
-            for (char vowel : vowels) {
-                if (strChar != vowel) {
-                    continue;
-                }
+        std::unordered_set<char> vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        std::vector<char> vowelChars;
 
-                vowelCharactersOnly.push_back(strChar);
+        for (char c : s) {
+            if (vowels.count(c)) {
+                vowelChars.push_back(c);
             }
         }
 
-        std::sort(vowelCharactersOnly.begin(), vowelCharactersOnly.end());
+        std::sort(vowelChars.begin(), vowelChars.end());
 
-        int vowelCharactersOnlyIdx = 0;
-        for (int i = 0; i < s.length(); i++) {
-            bool foundVowel = false;
-            for (char vowel : vowels) {
-                if (s[i] != vowel || foundVowel) {
-                    continue;
-                }
-
-                s[i] = vowelCharactersOnly.at(vowelCharactersOnlyIdx++);
-                foundVowel = true;
+        int idx = 0;
+        for (char &c: s) {
+            if (vowels.count(c)) {
+                c = vowelChars.at(idx++);
             }
         }
 
