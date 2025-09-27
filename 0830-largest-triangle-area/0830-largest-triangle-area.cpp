e34fdfd1184d782141d54086e31ef439afa5d732
Saif Ul Islam <k180307@nu.edu.pk>
2025-09-27 07:25:54 +0500

    Time: 2 ms (39.9%), Space: 10.4 MB (86.95%) - LeetHub

--- 0830-largest-triangle-area/0830-largest-triangle-area.cpp
+++ 0830-largest-triangle-area/0830-largest-triangle-area.cpp
@@ -3,20 +3,10 @@ public:
     double largestTriangleArea(vector<vector<int>>& points) {
         int n = points.size();
         double maxArea = 0;
-        for (int i = 0; i < n - 2; i++) {
-            for (int j = i + 1; j < n - 1; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    int x1 = points[i][0];
-                    int x2 = points[j][0];
-                    int x3 = points[k][0];
-                    int y1 = points[i][1];
-                    int y2 = points[j][1];
-                    int y3 = points[k][1];
-                    double tmpArea = abs(x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1-y2));
-                    maxArea = max(maxArea, 0.5 * tmpArea);
-                }
-            }
-        }
+        for (int i = 0; i < n - 2; i++)
+            for (int j = i + 1; j < n - 1; j++)
+                for (int k = j + 1; k < n; k++)
+                    maxArea = max(maxArea, 0.5 * (double)abs(points[i][0]*(points[j][1] - points[k][1]) + points[j][0]*(points[k][1] - points[i][1]) + points[k][0]*(points[i][1]-points[j][1])));
 
         return maxArea;
     }
