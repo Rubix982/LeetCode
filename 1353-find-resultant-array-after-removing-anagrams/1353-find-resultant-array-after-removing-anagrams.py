53170f40a64ded8d8e230e1f4abc41cc1a9402a1
Saif Ul Islam <k180307@nu.edu.pk>
2025-10-13 07:37:02 +0500

    Time: 2 ms (91.39%), Space: 17.7 MB (76.67%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.py
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.py
@@ -3,12 +3,15 @@ from collections import Counter
 class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
         res = []
-        prev_counter = None
-
-        for word in words:
-            curr_counter = Counter(word)
-            if curr_counter != prev_counter:
-                res.append(word)
-                prev_counter = curr_counter
-        return res
+        prev = None
         
+        for word in words:
+            # Canonical representation using sorted characters
+            curr = ''.join(sorted(word))
+            
+            if curr != prev:
+                res.append(word)
+                prev = curr
+        
+        return res
+            
